44f4af2e4443095f28a5694e1f662eefd0728428
SK Sabir Ali <sabir.cn@gmail.com>
2013-10-24 11:07:18 +0530

    updated

--- templates/js/titan-featured-menu.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-	$( ".listingwrapper" ).each(function() {
-		var titanform = $(this).find('.appointment-forms');
-		$(this).find(".appointment-forms-link").click(function() {
-				$(titanform).dialog({width:500});
-				return false;
-		});
-	});
-});
